3c26a68ac8a2a6e8028e3796c71cefcb1b37eb2b
mazen jaafar <mazenjaafar997@gmail.com>
2026-04-08 13:47:10 +0300

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://realpython.github.io/fake-jobs/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+jobs = soup.find_all("div", class_="card-content")
+
+job_list = []
+
+for job in jobs:
+    title = job.find("h2", class_="title").text.strip()
+    company = job.find("h3", class_="company").text.strip()
+    location = job.find("p", class_="location").text.strip()
+    
+    job_list.append({
+        "Title": title,
+        "Company": company,
+        "Location": location
+    })
+
+df = pd.DataFrame(job_list)
+df.to_csv("jobs.csv", index=False)
+
+print("تم استخراج البيانات بنجاح!")
